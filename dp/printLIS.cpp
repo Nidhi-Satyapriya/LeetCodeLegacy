bf7985c852af77f5f1ee5a65cdc832cd13e425dd
Nidhi Satyapriya <satyapriyanidhi@gmail.com>
2025-06-17 12:54:53 +0530

    dp: print LIS

---
+++ dp/printLIS.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> getLIS(vector<int>& arr) {
+    int n = arr.size();
+    vector<int> dp(n, 1), prev(n, -1);
+    
+    int maxi = 1, ind = 0;
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (arr[j] < arr[i] && dp[j] + 1 > dp[i]) {
+                dp[i] = dp[j] + 1;
+                prev[i] = j;
+            }
+        }
+        if (dp[i] > maxi) {
+            maxi = dp[i];
+            ind = i;
+        }
+    }
+
+    // Reconstruct LIS
+    vector<int> lis;
+    while (ind != -1) {
+        lis.push_back(arr[ind]);
+        ind = prev[ind];
+    }
+    reverse(lis.begin(), lis.end());
+    return lis;
+}
