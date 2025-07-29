8a69256e2ae08a1bfd2b7d2b7eace413b58018d6
Nidhi Satyapriya <satyapriyanidhi@gmail.com>
2025-07-29 16:02:31 +0530

    Binary Search Practice

---
+++ bs/rotatedSearch.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int st = 0, ed = nums.size() - 1;
+
+        while (st <= ed) {
+            int mid = st + (ed - st) / 2;
+
+            if (nums[mid] == target) return mid;
+
+            // Left half is sorted
+            if (nums[st] <= nums[mid]) {
+                if (nums[st] <= target && target < nums[mid]) {
+                    ed = mid - 1;
+                } else {
+                    st = mid + 1;
+                }
+            }
+            // Right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[ed]) {
+                    st = mid + 1;
+                } else {
+                    ed = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
